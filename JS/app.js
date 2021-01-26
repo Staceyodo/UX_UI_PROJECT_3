d464b2ff13e090d75baf025b76a9cbbd72ad0484
Marialuisa Yazar <marialuisa.yazar@gmail.com>
2021-01-25 21:04:24 -0800

    added add task button

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,25 @@ $(document).ready(function () {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
+
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".new-task").click(function(){
+        $("#empty").append('<div  class="task-card container card col-md-3 m-3" id="task">'+
+                            '<h3 class="card-title">Task</h3>'+
+                            '<div class="card" id="task-description">'+
+                            '<p class="card-text">Create harmony by 01/30/21</p>'+
+                            '</div>'+
+                            '</div>'+
+                            '<div id="empty">'+  
+                            '</div>');
+    });
+});
